6b64ec0b8a62c81931173e4a246efd201d6eec72
Jason Paller-Rzepka <jasonpr@mit.edu>
2015-06-19 10:43:27 -0400

    Add docstrings to regex.py.

--- pylexparse/regex.py
+++ pylexparse/regex.py
@@ -1,22 +1,23 @@
 """Tools for parsing a regular expression into a Pattern."""
 
-# TODO(jasonpr): Put add docstrings everywhere!
-
 import collections
 import string
 
 import pattern as p
 
-
+# Characters that represent themselves in a regular expression.
 _CHAR_LITERALS = string.ascii_letters + string.digits + '!"#%&\',-/:;<=>@_`~'
+# Characters that represent themselves inside a square-bracket expression.
 _GROUP_CHARS = string.ascii_letters + string.digits + '!"#$%&\'()*+,./:;<=>?@[^_`{|}~'
 
 class _CharSource(object):
+    """An input source with getc() and ungetc() equivalents."""
     def __init__(self, iteratable):
         self._iterator = iter(iteratable)
         self._put_chars = collections.deque()
 
     def get(self):
+        """Get the next character from the input stream."""
         if self._put_chars:
             return self._put_chars.pop()
         try:
@@ -26,18 +27,27 @@ class _CharSource(object):
             return None
 
     def put(self, char):
+        """Put a character back onto the input stream.
+
+        Characters are put back in LIFO order.
+        """
         self._put_chars.append(char)
 
 
 def parse_regex(regex_string):
+    """Convert a regular expression string into a Pattern."""
     return _parse_regex(_CharSource(regex_string))
 
 
+# The following _parse_* methods form a recursive descent parser
+# that respect the order of operations in a regular expression.
 def _parse_regex(source):
+    """Parse any regex into a Pattern."""
     return _parse_alternation(source)
 
 
 def _parse_alternation(source):
+    """Parse an alternation expression, like 'ab|cd|ef'."""
     parts = []
     # Act as though the last character was a '|', so we get the
     # initial element of the alternation.
@@ -52,6 +62,7 @@ def _parse_alternation(source):
 
 
 def _parse_concatenation(source):
+    """Parse a concatenation expression, like 'abc' or 'a(b|c)d*'."""
     parts = []
     duplication = _parse_duplication(source)
     # If we're expecting a concatenation, there MUST be at least
@@ -65,6 +76,7 @@ def _parse_concatenation(source):
 
 
 def _parse_duplication(source):
+    """Parse a duplication expression, like 'a*' or '(a|b){3,5}'."""
     duplicated = _parse_parenthesization(source)
     if not duplicated:
         return None
@@ -101,6 +113,11 @@ def _parse_duplication(source):
 
 
 def _parse_parenthesization(source):
+    """Parse a parenthesization pattern, like '(a|b)' or '[ab]' or 'a'.
+
+    Note that '[ab]' is a parenthesization, since it is equivalent
+    to '([ab])'.  Similarly, 'a' is equivalent to '(a)'.
+    """
     first_char = source.get()
     if first_char == '(':
         enclosed_regex = _parse_regex(source)
@@ -115,6 +132,10 @@ def _parse_parenthesization(source):
 
 
 def _parse_group(source):
+    """Parse a group pattern, like '[abc]' or 'a'.
+
+    Note that 'a' is a group, since 'a' is equivalent to '[a]'.
+    """
     first_char = source.get()
     if first_char == '[':
         group_chars = _parse_group_chars(source)
@@ -128,6 +149,11 @@ def _parse_group(source):
     return _parse_atom(source)
 
 def _parse_group_chars(source):
+    """Parse the characters from a group specification.
+
+    This is just a string of characters allowable in a group specification.
+    For example, a valid parse is 'aA1.?', since '[aA1.?]' is a valid group.
+    """
     chars = []
     next_char = source.get()
     while next_char and next_char in _GROUP_CHARS:
@@ -138,6 +164,10 @@ def _parse_group_chars(source):
 
 
 def _parse_atom(source):
+    """Parse a single regex atom.
+
+    An atom is a period ('.'), a character literal, or an escape sequence.
+    """
     char = source.get()
 
     if not char:
@@ -158,6 +188,10 @@ def _parse_atom(source):
         return None
 
 def _parse_positive_int(source):
+    """Parse a positive integer.
+
+    That is, parse a sequence of one or more digits.
+    """
     digits = []
     next_char = source.get()
     assert next_char and next_char in string.digits
